a6b87752a53188c1a8a906e10a0656a7afb3508c
Lisa <misslisazhou@gmail.com>
2016-07-02 15:17:11 -1000

    store response headers in hash table

--- client.js
+++ client.js
@@ -9,7 +9,7 @@ if (host === undefined){
   console.log('Please add a URL to request. See example: "node client.js www.google.com"');
   process.exit();
 }
-var method = 'HEAD';
+var methodReq = 'GET';
 var uriRequest = '/';
 
 //seperate uri from host name if '/' is present
@@ -25,23 +25,27 @@ if (host === 'localhost') {
 var client = net.createConnection(port, host, function () {
   client.setEncoding('utf8');
   console.log('connected to server!');
-  var res;
-
   var now = new Date();
-  client.write(method + ' ' + uriRequest +' HTTP/1.0\r\n' +
-    'DATE: ' + now.toUTCString() + '\r\n' +
-    'HOST: ' + host + ' \r\n' +
-    'USER-AGENT: meeeeeeee');
+  client.write(methodReq + ' ' + uriRequest +' HTTP/1.1\n' +
+    'Date: ' + now.toUTCString() + '\n' +
+    'Host: ' + host + '\n' +
+    'User-Agent: meeeeeeee' + '\n\n');
   client.on('data', function (data) {
-    console.log('data' + Date.now());
     // Log the response from the HTTP server
-    // var responseHeaders = [];
-    // console.log(data.substring(0, data.indexOf('<!DOCTYPE html>')));
-    res = data.split('\n');
-    console.log(res);
-    console.log(res[0]);
-    // console.log(data);
-    // process.stdout.write(data);
+    var dataArr = data.split('\n'); //convert all requested data into an array
+    var statusArr = dataArr[0].split(' '); //convert status line into an array
+    var headersArr = data.split('\n').slice(1, dataArr.indexOf('')); //put all response headers in seperate array
+    var responseHeaders = {
+      'HTTP-Version': statusArr[0],
+      'Status Code': statusArr[1],
+      'Reason Phrase': statusArr[2]
+    };
+    headersArr.forEach(function (elem) {
+      var key = elem.substring(0, elem.indexOf(':') + 1);
+      responseHeaders[key] = elem.substring(elem.indexOf(':') + 2);
+    });
+    console.log(responseHeaders);
+    console.log(data);
     client.end();
   });
 
