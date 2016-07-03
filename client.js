f06408ea9dc100c6fa9a37f72c54830a9343b67e
Lisa <misslisazhou@gmail.com>
2016-07-02 17:03:32 -1000

    set CLI options and display headers only if requested

--- client.js
+++ client.js
@@ -3,13 +3,17 @@ var net = require('net');
 //assign [node client.js path] to variable
 var input = process.argv;
 
-var port = 80;
 var host = input[2];
 if (host === undefined){
-  console.log('Please add a URL to request. See example: "node client.js www.google.com"');
+  console.log('Please use this format: node client.js [url] [request method] [Headers Only (Yes/No)] [PORT]\nExample: "node client.js www.google.com GET No 80"');
   process.exit();
 }
-var methodReq = 'GET';
+var methodReq = input[3] || 'GET';
+var headersOnly = input[4];
+if (headersOnly !== undefined){
+  headersOnly = input[4].toLowerCase();
+}
+var port = input[5] || 80;
 var uriRequest = '/';
 
 //seperate uri from host name if '/' is present
@@ -24,7 +28,6 @@ if (host === 'localhost') {
 
 var client = net.createConnection(port, host, function () {
   client.setEncoding('utf8');
-  console.log('connected to server!');
   var now = new Date();
   client.write(methodReq + ' ' + uriRequest +' HTTP/1.1\n' +
     'Date: ' + now.toUTCString() + '\n' +
@@ -34,7 +37,9 @@ var client = net.createConnection(port, host, function () {
     // Log the response from the HTTP server
     var dataArr = data.split('\n'); //convert all requested data into an array
     var statusArr = dataArr[0].split(' '); //convert status line into an array
+    var headBodySplit = data.split(/(\r\n\r\n|\n\n)/);
     var headersArr = data.split('\n').slice(1, dataArr.indexOf('')); //put all response headers in seperate array
+    //store response headers in hashtable
     var responseHeaders = {
       'HTTP-Version': statusArr[0],
       'Status Code': statusArr[1],
@@ -44,8 +49,12 @@ var client = net.createConnection(port, host, function () {
       var key = elem.substring(0, elem.indexOf(':') + 1);
       responseHeaders[key] = elem.substring(elem.indexOf(':') + 2);
     });
-    console.log(responseHeaders);
-    console.log(data);
+    // process.stdout.write(data);
+    if (headersOnly === 'yes'){
+      process.stdout.write(headBodySplit[0]);
+    }else{
+      process.stdout.write(data);
+    }
     client.end();
   });
 
